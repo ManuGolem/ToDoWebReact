7c25ccf1fc8826c3330ec06b433b51933952dfd6
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-02 20:46:28 -0300

    some class

--- src/Components/Tarea.jsx
+++ src/Components/Tarea.jsx
@@ -1,5 +1,13 @@
-export function Tareas({text}){
-    return(
-        <div className="w-[250px] h-[250px] bg-lime-100 rounded-2xl p-5 grid justify-center items-center">Tareas {text}</div>
-    )
+export function Tarea({ text }) {
+	return (
+		<div className="bg-lime-100 rounded-2xl p-5 grid justify-center items-center">
+			<h1 className="text-5xl font-bold text-center">Tareas {text}</h1>
+			<div className="mt-5 text-center">
+				<p className="text-center text-gray-400 font-bold">Sin tareas</p>
+				<button className="pt-6">
+                    <img className="w-[40px] text-white hover:scale-125 hover:cursor-pointer"src="/add.svg"/>
+                </button>
+			</div>
+		</div>
+	);
 }
