5f30dfe374ebcb3bd675bb5505fe5f4886b531d9
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-02 20:46:22 -0300

    some class

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,10 @@
-import { Button } from "./Button"
+import { ButtonLink } from "./ButtonLink"
 export function Footer(){
     return(
-        <div className="flex justify-end gap-3 m-5 mt-auto">
-            <Button name="Github" img="/github-mark.png" href="https://github.com/ManuGolem"/>
-            <Button name="Linkedin" img="/linkedin.png" href="https://www.linkedin.com/in/manuel-carreras-b05414265/"/>
-            <Button name="Manuel Carreras" href="https://manuelcarreras.netlify.app/"/>
+        <div className="flex justify-end gap-3 m-5 mt-auto first:">
+            <ButtonLink name="Manuel Carreras" href="https://manuelcarreras.netlify.app/" className="text-black mr-auto"/>
+            <ButtonLink name="Github" img="/github-mark.png" href="https://github.com/ManuGolem"/>
+            <ButtonLink name="Linkedin" img="/linkedin.png" href="https://www.linkedin.com/in/manuel-carreras-b05414265/"/>
         </div>
     )
 }
