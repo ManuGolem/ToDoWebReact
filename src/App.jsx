b82a88d3f553edd37401985882a630688d446469
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-02 01:18:56 -0300

    configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import { Tareas } from "./Tareas";
+export function App() {
+	return (
+		<>
+			<h1 className="text-center font-bold text-7xl text-violet-500 ">
+				To <span className="text-lime-400">Do</span> web
+			</h1>
+            <div className="flex justify-center m-10 gap-2">
+                {/* Tareas Diarias */}
+                <Tareas text="Diarias"/>
+                {/* Tareas Semanales */}
+                <Tareas text="Semanales"/> 
+            </div>
+		</>
+	);
 }
-
-export default App
---
+++ src/Tareas.jsx
@@ -0,0 +1,5 @@
+export function Tareas({text}){
+    return(
+        <div className="w-[250px] h-[250px] bg-lime-100 rounded-2xl p-5 grid justify-center items-center">Tareas {text}</div>
+    )
+}
