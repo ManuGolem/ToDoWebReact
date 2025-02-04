f7dff87a5cb51a82854b3427f57c04f4064c7ac8
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-04 20:42:27 -0300

    no me acuerdo

--- src/Components/CampoTarea.jsx
+++ src/Components/CampoTarea.jsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
 import { Button } from "./Button";
-export function CampoTarea({ id, remove }) {
-	const [check,setCheck]=useState(false)
-	const clasesInputBase="outline-none ml-auto text-center rounded-2xl hover:bg-lime-200 "
+export function CampoTarea({ id, remove, texto }) {
+	const [check, setCheck] = useState(false);
+	const clasesInputBase = "outline-none ml-auto text-center rounded-2xl hover:bg-lime-200 grow";
 	function removeTarea() {
 		remove(id);
 	}
-	function completarTarea(){
-		setCheck(!check)
+	function completarTarea() {
+		setCheck(!check);
 	}
 	return (
 		<div className="flex justify-center">
-			<Button img={check?"checkbox-check.svg":"/checkbox-unchecked.svg"} funcion={completarTarea} tipo="checkbox" alt="checkbox"/>
-			<input type="text" placeholder="Escribe aqui" className={(check?"line-through text-lime-500 hover:text-black" : "").concat(" ",clasesInputBase)}></input>
+			<Button
+				img={check ? "checkbox-check.svg" : "/checkbox-unchecked.svg"}
+				funcion={completarTarea}
+				tipo="checkbox"
+				alt="checkbox"
+			/>
+			<input
+				type="text"
+				placeholder="Escribe aqui"
+				className={(check ? "line-through text-lime-500 hover:text-black" : "").concat(" ", clasesInputBase)}
+				defaultValue={texto}
+			></input>
 			<Button img="/delete.svg" funcion={removeTarea} tipo="remove" alt="remove" />
 		</div>
 	);
