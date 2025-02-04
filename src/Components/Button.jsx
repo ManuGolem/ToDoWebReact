e11fbd2ba73183b1a60958c29fb55fbf39c967ca
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-04 15:32:30 -0300

    add object to decide the type

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,6 +1,11 @@
 export function Button({img,funcion,tipo,alt}) {
+	const botonTipo= {
+		"add":"mt-6 hover:bg-violet-500",
+		"remove" :"ml-auto hover:bg-red-500",
+		"checkbox" :""
+	}
     const clasesBase="hover:scale-125 hover:cursor-pointer hover:rounded-[8px] duration-200 active:scale-100"
-	const clasesBoton=tipo=="add"? "mt-6 hover:bg-violet-500".concat(" ",clasesBase) : "ml-auto hover:bg-red-500".concat(" ",clasesBase)
+	const clasesBoton=botonTipo[tipo].concat(" ",clasesBase)
     return (
 		<button className={clasesBoton} onClick={funcion}>
 			<img className="w-[40px]" src={img} alt={alt}/>
--- src/Components/CampoTarea.jsx
+++ src/Components/CampoTarea.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import { Button } from "./Button";
 export function CampoTarea({ id, remove }) {
+	const [check,setCheck]=useState(false)
+	const clasesInputBase="outline-none ml-auto text-center rounded-2xl hover:bg-lime-200 "
 	function removeTarea() {
 		remove(id);
 	}
+	function completarTarea(){
+		setCheck(!check)
+	}
 	return (
 		<div className="flex justify-center">
-			<input type="checkbox" className="w-[30px] border-none outline-none no-underline"></input>
-			<input type="text" placeholder="Escribe aqui" className="no-underline outline-none ml-auto text-center rounded-2xl hover:bg-lime-200"></input>
+			<Button img={check?"checkbox-check.svg":"/checkbox-unchecked.svg"} funcion={completarTarea} tipo="checkbox" alt="checkbox"/>
+			<input type="text" placeholder="Escribe aqui" className={(check?"line-through text-lime-500 hover:text-black" : "").concat(" ",clasesInputBase)}></input>
 			<Button img="/delete.svg" funcion={removeTarea} tipo="remove" alt="remove" />
 		</div>
 	);
