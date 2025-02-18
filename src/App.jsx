1b9e1052f7c8568fdd0de8bbc6afd43cd14d9e40
ManuGolem <manuelcarreras2013@gmail.com>
2025-02-17 22:35:40 -0300

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import { Tarea} from "./Components/Tarea";
-import { Footer} from "./Components/Footer";
+import { Tarea } from "./Components/Tarea";
+import { Footer } from "./Components/Footer";
 export function App() {
-	const fecha=Date.parse();
-	console.log(fecha)
-	return (
-		<>
-			<h1 className="text-center font-bold text-7xl text-violet-500 ">
-				To <span className="text-lime-400">Do</span> web
-			</h1>
+    return (
+        <>
+            <h1 className="text-center font-bold text-7xl text-violet-500 ">
+                To <span className="text-lime-400">Do</span> web
+            </h1>
             <div className="flex justify-center mt-10 sm:m-10 gap-2">
-                {/* Tareas Diarias */}
-                <Tarea text="Tareas Diarias"/>
-                {/* Tareas Semanales */}
-                {/* <Tarea text="Semanales"/>  */}
+                <Tarea text="Tareas Diarias" />
             </div>
-			<Footer/>
-		</>
-	);
+            <Footer />
+        </>
+    );
 }
